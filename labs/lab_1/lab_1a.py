787d6d7242f16210141f1be0b192f81b69ab66a2
Gagan Potturi <gaganpotturi@icloud.com>
2026-02-15 15:18:44 -0600

    personal introduction added to lab_1a-py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Gagan Potturi" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hello, my name is Gagan and I am a junoir from Texas. I love playing tennis and I am a vegetarian. I am also the Assistant Mechanical Lead fo rthe FRC team Pastabots(#9418). I really enjoy designing things with CAD and I love to build things with my hands.")
 if __name__ == "__main__":
     main()
